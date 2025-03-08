ec990c9d51cb402ed0e6fef755297a6c965a8126
Leandro <leandro.rufma@gmail.com>
2025-03-08 11:10:45 -0300

    .

--- src/client/localplayer.cpp
+++ src/client/localplayer.cpp
@@ -207,9 +207,7 @@ void LocalPlayer::onPositionChange(const Position& newPos, const Position& oldPo
     else if (m_autoWalkDestination.isValid() && newPos == m_lastAutoWalkPosition)
         autoWalk(m_autoWalkDestination);
 
-    if (isServerWalking()) {
-        m_serverWalk = false;
-    }
+    m_serverWalk = false;
 }
 
 void LocalPlayer::setStates(const uint32_t states)
